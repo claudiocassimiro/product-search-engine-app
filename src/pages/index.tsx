0d4813861e52e61781edb48bb54674b6a06a3e23
Claudio Cassimiro <days_surfing@live.com>
2023-04-12 16:13:55 -0300

    feat: add component FiltersAndSearchResults

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import FiltersAndSearchResults from "@/components/FiltersAndSearchResults";
+// import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,7 +15,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" /> {/* Trocar esse favicon */}
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          {/* <Image /> */}
+          <h1 className={styles.title}>EasyBuy</h1>
+          {/* Burger Button com links para GitHub e LinkedIn */}
+        </header>
+        <FiltersAndSearchResults />
+      </main>
     </>
   );
 }

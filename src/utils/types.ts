f575bc7cd0e685faaefde6de1fbcf16780c70bc1
Claudio Cassimiro <days_surfing@live.com>
2023-04-12 16:12:14 -0300

    feat: implements file to centralize all types used on app

---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+export type Products = {
+  productName: string | null;
+  productPrice: string | null;
+  productImage: string | null;
+  productLink: string | null;
+  productCategory: string | null;
+};

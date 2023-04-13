4b8b1ceadfa20247953c928dfc06640c7e8412e8
Claudio Cassimiro <days_surfing@live.com>
2023-04-13 12:43:12 -0300

    refactor: implements some lines of code to work on production env

--- src/pages/api/getMLProducts.ts
+++ src/pages/api/getMLProducts.ts
@@ -1,16 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer";
+import puppeteerType from "puppeteer";
+import chromeType from "chrome-aws-lambda";
 import { Products } from "@/utils/types";
 
 type MeliProducts = Products;
+
+let chrome = {} as typeof chromeType;
+let puppeteer: typeof puppeteerType;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
 export default async function getMLProducts(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { web, category, inputValue } = req.query;
+
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     const meliProducts = [] as MeliProducts[];
 

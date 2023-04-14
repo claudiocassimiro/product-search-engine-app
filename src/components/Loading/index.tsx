ec5903e93b1cdbb417ad533e2a4c97241af9bc66
Claudio Cassimiro <days_surfing@live.com>
2023-04-14 19:55:09 -0300

    refactor: simplify the code and add data aos

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
+const loadingText = [
+  "Colocando gasolina no Robô...",
+  "Passando Parâmetros...",
+  "Chutando para funcionar...",
+  "Buscando...",
+];
+
 const Loading = () => {
   const [step, setStep] = useState(0);
 
@@ -14,17 +21,12 @@ const Loading = () => {
   }, [step]);
 
   return (
-    <div className={styles.containerLoading}>
-      {step === 0 ? (
-        <p className={styles.loadingText}>Colocando gasolina no Robô...</p>
-      ) : null}
-      {step === 1 ? (
-        <p className={styles.loadingText}>Passando Parâmetros...</p>
-      ) : null}
-      {step === 2 ? (
-        <p className={styles.loadingText}>Chutando para funcionar...</p>
-      ) : null}
-      {step >= 3 ? <p className={styles.loadingText}>Buscando...</p> : null}
+    <div
+      data-aos="flip-up"
+      data-aos-duration="800"
+      className={styles.containerLoading}
+    >
+      <p className={styles.loadingText}>{loadingText[step]}</p>
     </div>
   );
 };

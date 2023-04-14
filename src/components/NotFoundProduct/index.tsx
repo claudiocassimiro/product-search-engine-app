45a64ec219a2675c3a193f022156b44f2076fac1
Claudio Cassimiro <days_surfing@live.com>
2023-04-14 18:43:51 -0300

    feat: implement component NotFoundProduct

---
+++ src/components/NotFoundProduct/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+
+const NotFoundProduct = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.containerMessage}>
+        Ops! Parece que não existe resultados para essa pesquisa, tente buscar
+        por outro produto!
+      </p>
+      <Image
+        src="/notfound-image.webp"
+        alt="Notfound image"
+        width={250}
+        height={250}
+      />
+    </div>
+  );
+};
+
+export default NotFoundProduct;

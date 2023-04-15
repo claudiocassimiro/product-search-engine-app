84a56836b46684eddeb68e03236fd37f40b25ade
Claudio Cassimiro <days_surfing@live.com>
2023-04-15 14:46:40 -0300

    feat: add some config to fix jest

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 const customJestConfig = {
+  testEnvironment: "jsdom",
   moduleDirectories: ["node_modules", "<rootDir>/"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   testEnvironment: "jest-environment-jsdom",
 };
 module.exports = createJestConfig(customJestConfig);

feecf03992bad5a1784cff49d971588d699e512b
Claudio Cassimiro <days_surfing@live.com>
2023-04-17 18:55:31 -0300

    feat: add railway to config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "http2.mlstatic.com", "i.zst.com.br"],
+    domains: [
+      "localhost",
+      "http2.mlstatic.com",
+      "i.zst.com.br",
+      "you-easy-buy.up.railway.app",
+    ],
   },
   env: {
     API_URL: process.env.API_URL,
